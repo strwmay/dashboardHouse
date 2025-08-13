9650455d5599fd017e97c93e874ae61c2266abd6
May <senaidev.may@gmail.com>
2025-08-13 15:03:42 -0300

    display flex (n funcionou)

--- House/src/App.jsx
+++ House/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"; 
 import LivingRoom from './components/LivingRoom';
 import Bedroom from './components/Bedroom';
 import Garage from './components/Garage';
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="d-flex flex-column align-items-center mt-5">
       <h1>Smart House</h1>
-      <div className="container">
+      <div className="d-flex flex-wrap justify-content-center gap-4 mt-4">
         <LivingRoom />
         <Bedroom />
         <Garage />
